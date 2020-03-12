d1ba142083ad159aeff45f89f383311d6cda926c
qmphy99@gmail.com <qmphy99@gmail.com>
2020-03-12 14:45:19 +0100

    init

--- Program.cs
+++ Program.cs
@@ -7,6 +7,9 @@ namespace GitNA20
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Hello World!");
+
         }
     }
 }

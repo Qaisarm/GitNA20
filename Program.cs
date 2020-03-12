e8f323f81e9f3a255f20452c4d8a32962d045f31
qmphy99@gmail.com <qmphy99@gmail.com>
2020-03-12 14:39:09 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitNA20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
